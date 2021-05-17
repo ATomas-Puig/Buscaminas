8568a71591995e1d489f79e2ce80d7961ca52b25
Antonio <atomas@elpuig.xeill.net>
2021-05-17 17:40:33 +0200

    Added tablero class

---
+++ src/main/java/model/main.java
@@ -0,0 +1,9 @@
+package model;
+
+public class main {
+    public static void main(String[] args) {
+        Tablero tablero = new Tablero();
+        tablero.initTablero();
+        System.out.println(tablero);
+    }
+}
